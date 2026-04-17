564de7f5f698fb2cda9f5baad242d118d831c1a0
Jojiasd <112561277+Jojiasd@users.noreply.github.com>
2026-04-17 10:41:26 +0530

    Update getStudents.js

--- std id app/api/getStudents.js
+++ std id app/api/getStudents.js
@@ -15,8 +15,8 @@ module.exports = async (req, res) => {
     const data = await fetch(files.blobs[0].url);
     const students = await data.json();
 
-    res.status(200).json(students);
+    return res.status(200).json(students);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch students' });
+    return res.status(500).json({ error: 'Failed to fetch students' });
   }
 };
